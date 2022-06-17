4f798556fe27e2015da249289238e59f7a956640
Guillermo Pérez <guiller.gpf@gmail.com>
2022-06-17 11:21:05 +0200

    updates

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,6 +5,5 @@ router.use('/user', require('./user.routes'))
 router.use('/plans', require('./plans.routes'))
 router.use('/upload', require('./upload.routes'))
 router.use('/ads', require('./ads.routes'))
-router.use('/messages', require('./chat.routes'))
 
 module.exports = router
